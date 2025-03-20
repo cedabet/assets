123caa3e3f74964ad9f411269d20483355b8906f
cedabet <cedabetcasino@gmail.com>
2025-03-20 20:35:42 +0300

    Update custom.js

--- custom.js
+++ custom.js
@@ -52,18 +52,22 @@ sectionTitleElements.forEach(title => {
         title.addEventListener('click', () => {
             window.location.href = '/en/casino/group/new-releases';
         });
+		 }
 		  if (title.textContent.trim().includes('New Releases')) {
         title.addEventListener('click', () => {
             window.location.href = '/en/casino/group/new-releases';
         });
+			  }
 			  	  if (title.textContent.trim().includes('High RTP')) {
         title.addEventListener('click', () => {
             window.location.href = '/en/casino/group/enhanced-rtp';
         });
+					  }
 					  	  if (title.textContent.trim().includes('Buy Bonus')) {
         title.addEventListener('click', () => {
             window.location.href = '/en/casino/group/bonus-buy';
         });
+							  }
 			 
     }
 });
