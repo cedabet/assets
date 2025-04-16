8786fb83882def8b4685ac91b4ab0ddfa1476edf
cedabet <cedabetcasino@gmail.com>
2025-04-16 14:28:12 +0300

    Update customEN.js

--- customEN.js
+++ customEN.js
@@ -27,31 +27,7 @@
 
       
 
-        var sidebarBtn = document.querySelector(".sidebar__btn");
-
-        sidebarBtn.addEventListener("click", function () {
-            var sportspath2 = window.location.pathname;
-		   var sidebarLogo = document.querySelector(".header__logo");
-
-            if (sportspath2 === "/en/sportsbook") {
-                var sidebar = document.getElementById("sidebar");
-             
-                if (sidebar.className.indexOf("active") !== -1) { // 'active' sınıfı yoksa
-
-                    sidebar.className = ""; // Tüm sınıfları temizle
-                    sidebar.classList.add("sidebar", "active"); 
-	
-                    sidebarLogo.style.setProperty('display', 'block', 'important');
-                } else {
-
-                    sidebar.className = ""; // Tüm sınıfları temizle
-                    sidebar.classList.add("sidebar"); // Sadece 'sidebar' sınıfını ekle
-		    sidebarLogo.style.setProperty('display', 'none', 'important'); 
-
-                }
-
-            }
-        });
+        
 
      
 
@@ -79,11 +55,41 @@
 
             else if (path === "/en/sportsbook") {
                 var sportspath = window.location.pathname;
+		      var sidebarLogo = document.querySelector(".header__logo");
+
                 if (sportspath === "/en/sportsbook") {
                     var sidebar = document.getElementById("sidebar");
                     sidebar.className = "";
                     sidebar.classList.add("sidebar", "active");
+var sidebarBtn = document.querySelector(".sidebar__btn");
+
+        sidebarBtn.addEventListener("click", function () {
+            var sportspath2 = window.location.pathname;
+		   var sidebarLogo = document.querySelector(".header__logo");
+
+            if (sportspath2 === "/en/sportsbook") {
+             
+                if (sidebar.className.indexOf("active") !== -1) { // 'active' sınıfı yoksa
+
+                    sidebar.className = ""; // Tüm sınıfları temizle
+                    sidebar.classList.add("sidebar", "active"); 
+	
+                    sidebarLogo.style.setProperty('display', 'block', 'important');
+                } else {
+
+                    sidebar.className = ""; // Tüm sınıfları temizle
+                    sidebar.classList.add("sidebar"); // Sadece 'sidebar' sınıfını ekle
+		    sidebarLogo.style.setProperty('display', 'none', 'important'); 
+
                 }
+
+            }
+        });
+			
+                }
+		    else{
+		       sidebarLogo.style.setProperty('display', 'none', 'important'); 
+		    }
              
 
             }
