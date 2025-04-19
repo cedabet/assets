f417856b5742369d8a546ac7cb5c2aa692bab833
cedabet <cedabetcasino@gmail.com>
2025-04-19 04:00:14 +0300

    Update customTR.js

--- customTR.js
+++ customTR.js
@@ -32,8 +32,8 @@ document.head.appendChild(link);
             
                     var sidebar = document.getElementById("sidebar");
                     sidebar.className = "";
-                    sidebar.classList.add("sidebar", "sidebar--sport-active");
-		    sidebar.classList.add("active");
+                    sidebar.classList.add("sidebar", "sidebar--sport-active", "active");
+
                     var sidebarLogo = document.querySelector(".header__logo");
                     if (sidebarLogo) {
                      //   sidebarLogo.style.setProperty('display', 'none', 'important');
@@ -47,7 +47,7 @@ document.head.appendChild(link);
              
                     var sidebar = document.getElementById("sidebar");
                     sidebar.className = "";
-                    sidebar.classList.add("sidebar", "sidebar--sport-active");
+                    sidebar.classList.add("sidebar", "sidebar--sport-active", "active");
                     var sidebarLogo = document.querySelector(".header__logo");
                     if (sidebarLogo) {
                      //   sidebarLogo.style.setProperty('display', 'none', 'important');
