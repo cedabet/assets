ec5c08716b9c7d53958fae11dd855a7ce851f277
cedabet <cedabetcasino@gmail.com>
2025-04-25 02:59:42 +0300

    Update customEN.js

--- customEN.js
+++ customEN.js
@@ -280,7 +280,7 @@ function createVipExperience() {
    newDiv.id = 'vip-container';
    newDiv.style.backgroundColor = '#03121a';
    newDiv.style.width = '100%';
-   newDiv.style.height = '569px';
+   newDiv.style.height = '50px';
    newDiv.style.position = 'relative';
    newDiv.style.borderRadius = '8px';
    newDiv.style.overflow = 'hidden';
