792cd27baa6eeed6633ac7a6be726b025942891e
cedabet <cedabetcasino@gmail.com>
2025-05-09 18:36:13 +0300

    Update customEN.js

--- customEN.js
+++ customEN.js
@@ -1112,7 +1112,7 @@ function createLeagueSection() {
   `;
 
 
-  const targetSection = document.getElementById('mini-games-wrapper-2');
+  const targetSection = document.getElementById('custom-section-7');
   if (targetSection && targetSection.parentNode) {
     targetSection.parentNode.insertBefore(wrapper, targetSection.nextSibling);
   } else {
