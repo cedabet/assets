65e16b45bdb28d4538a2ca1ef9e2302cea3b9ab1
cedabet <cedabetcasino@gmail.com>
2025-05-14 19:23:20 +0300

    Update customRU.js

--- customRU.js
+++ customRU.js
@@ -189,7 +189,9 @@ function startLiveFakeBets() {
         <div class="xtable__coin gap-1 text-secondary">
           <span></span>
           <span class="xtable__coin text-white">${bet}</span>
-          <div class="instrument-icon-wrapper"><svg class="svg-icon"><path fill="#EB0A29"></path></svg></div>
+           <div class="instrument-icon-wrapper "><svg fill="none" viewBox="0 0 96 96" class="svg-icon "><path d="M48 96c26.51 0 48-21.49 48-48S74.51 0 48 0 0 21.49 0 48s21.49 48 48 48Z" fill="#EB0A29">
+	  </path><path d="M71.4 55.08c-2.72 10.96-12.48 19.6-25.16 19.6H34.72V59.24l-5.76 3.36v-7.2l5.76-3.36V46.2l-5.76 3.28v-7.2l5.76-3.36v-17.6h11.36v11.04l13.2-7.6v7.28l-13.2 7.6v5.84l13.2-7.6v7.2l-13.2 7.6v12h1.2c6.4 0 12.56-4.48 14.32-12.24l9.76 2.64h.04Z" fill="#fff">
+   </path></svg></div>
         </div>
       </td>
       <td class="text-right"><div class="xtable__text text-white">x${multiplier}</div></td>
@@ -197,7 +199,9 @@ function startLiveFakeBets() {
         <div class="xtable__coin text-secondary gap-1">
           <span></span>
           <span>${profit}</span>
-          <div class="instrument-icon-wrapper"><svg class="svg-icon"><path fill="#EB0A29"></path></svg></div>
+          <div class="instrument-icon-wrapper "><svg fill="none" viewBox="0 0 96 96" class="svg-icon "><path d="M48 96c26.51 0 48-21.49 48-48S74.51 0 48 0 0 21.49 0 48s21.49 48 48 48Z" fill="#EB0A29">
+	  </path><path d="M71.4 55.08c-2.72 10.96-12.48 19.6-25.16 19.6H34.72V59.24l-5.76 3.36v-7.2l5.76-3.36V46.2l-5.76 3.28v-7.2l5.76-3.36v-17.6h11.36v11.04l13.2-7.6v7.28l-13.2 7.6v5.84l13.2-7.6v7.2l-13.2 7.6v12h1.2c6.4 0 12.56-4.48 14.32-12.24l9.76 2.64h.04Z" fill="#fff">
+   </path></svg></div>
         </div>
       </td>
     `;
@@ -207,7 +211,7 @@ function startLiveFakeBets() {
       tbody.prepend(row);
 
       // Maksimum 100 satır tut, eskileri sil
-      if (tbody.children.length > 100) {
+      if (tbody.children.length > 10) {
         tbody.removeChild(tbody.lastChild);
       }
     }
