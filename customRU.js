2e107904266ab1f20d410f4944291fb5e63d63c7
cedabet <cedabetcasino@gmail.com>
2025-05-14 23:17:02 +0300

    Update customRU.js

--- customRU.js
+++ customRU.js
@@ -8,7 +8,7 @@ document.head.appendChild(link);
     let lastUrl = location.href;
     let isFirstLoad = true;
     if (isFirstLoad) {
-	    addFakeBetRow();
+	  
         loadVipFeatures();
         setTimeout(loadh2Title, 1000);
         addMenuElement();
@@ -151,111 +151,6 @@ function updateCopyrightYear() {
     });
 }
 
-function addFakeBetRow() {
-  function getRandomFromArray(arr) {
-    return arr[Math.floor(Math.random() * arr.length)];
-  }
-
-  function getCurrentTime() {
-    const now = new Date();
-    return now.toTimeString().split(" ")[0];
-  }
-
-  function getRandomFloat(min, max, decimals = 2) {
-    return parseFloat((Math.random() * (max - min) + min).toFixed(decimals));
-  }
-
-  function generateUniqueId() {
-    return Math.floor(Date.now() + Math.random() * 100000);
-  }
-
-  const games = [
-    "Floating Dragon", "Sweet Bonanza", "Gates of Olympus",
-    "Big Bass Bonanza", "Sugar Rush", "Starlight Princess", "Treasures of Aztec"
-  ];
-  const users = ["Скрытый", "User123", "LuckyGuy", "CryptoQueen", "AnonBet"];
-
-  const game = getRandomFromArray(games);
-  const user = getRandomFromArray(users);
-  const time = getCurrentTime();
-  const bet = getRandomFloat(0.1, 10);
-
-  let multiplier, profit;
-
-  if (Math.random() < 0.75) {
-    multiplier = 0.00;
-    profit = -bet;
-  } else {
-    multiplier = getRandomFloat(1.01, 5);
-    profit = parseFloat((bet * multiplier - bet).toFixed(2));
-  }
-
-  const profitClass = profit > 0
-    ? 'xtable__coin xtable__coin--green   gap-1 '
-    : 'xtable__coin text-secondary   gap-1 ';
-
-  const uniqueId = generateUniqueId();
-
-  const row = document.createElement('tr');
-  row.innerHTML = `
-    <td>
-        <div class="xtable__text cursor-pointer text-white text-truncate"><a>${game}</a></div>
-    </td>
-    <td>
-        <div class="xtable__text cursor-pointer">
-            <span class="d-flex align-items-center justify-content-center gap-1">
-                <svg class="svg-icon"><use href="/static/media/sprite.svg#hidden"></use></svg>${user}
-            </span>
-        </div>
-    </td>
-    <td>
-        <div class="xtable__text"><small>${time}</small></div>
-    </td>
-    <td class="text-right">
-        <div class="xtable__coin gap-1 text-secondary">
-            <span></span>
-            <span class="xtable__coin text-white" id="bet-amount-${uniqueId}">${bet.toFixed(2)}</span>
-            <div class="instrument-icon-wrapper">
-                <svg fill="none" viewBox="0 0 96 96" class="svg-icon">
-                    <path d="M48 96c26.51 0 48-21.49 48-48S74.51 0 48 0 0 21.49 0 48s21.49 48 48 48Z" fill="#EB0A29"></path>
-                    <path d="M71.4 55.08c-2.72 10.96-12.48 19.6-25.16 19.6H34.72V59.24l-5.76 3.36v-7.2l5.76-3.36V46.2l-5.76 3.28v-7.2l5.76-3.36v-17.6h11.36v11.04l13.2-7.6v7.28l-13.2 7.6v5.84l13.2-7.6v7.2l-13.2 7.6v12h1.2c6.4 0 12.56-4.48 14.32-12.24l9.76 2.64h.04Z" fill="#fff"></path>
-                </svg>
-            </div>
-        </div>
-    </td>
-    <td class="text-right">
-        <div class="xtable__text text-white">x${multiplier.toFixed(2)}</div>
-    </td>
-    <td>
-        <div class="${profitClass}">
-            <span></span>
-            <span id="win-amount-${uniqueId}">${profit.toFixed(2)}</span>
-            <div class="instrument-icon-wrapper">
-                <svg fill="none" viewBox="0 0 96 96" class="svg-icon">
-                    <path d="M48 96c26.51 0 48-21.49 48-48S74.51 0 48 0 0 21.49 0 48s21.49 48 48 48Z" fill="#EB0A29"></path>
-                    <path d="M71.4 55.08c-2.72 10.96-12.48 19.6-25.16 19.6H34.72V59.24l-5.76 3.36v-7.2l5.76-3.36V46.2l-5.76 3.28v-7.2l5.76-3.36v-17.6h11.36v11.04l13.2-7.6v7.28l-13.2 7.6v5.84l13.2-7.6v7.2l-13.2 7.6v12h1.2c6.4 0 12.56-4.48 14.32-12.24l9.76 2.64h.04Z" fill="#fff"></path>
-                </svg>
-            </div>
-        </div>
-    </td>
-  `;
-
-  const tbody = document.querySelector('table tbody');
-  if (tbody) {
-    // Eğer daha önce eklenmiş bir satır varsa (en üstteki), onu sil
-    if (tbody.firstElementChild) {
-      tbody.removeChild(tbody.firstElementChild);
-    }
-
-    // Yeni satırı ekle
-    tbody.prepend(row);
-  }
-}
-
-// Otomatik veri ekle (300ms aralıklarla)
-setInterval(() => {
-  addFakeBetRow();
-}, 300);
 
 
 function insertCedaTVButton() {
